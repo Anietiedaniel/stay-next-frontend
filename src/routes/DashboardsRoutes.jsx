38a7764337c27b1a817872fae9e99b436b98b003
AnietieDaniel <anietienteabasi@gmail.com>
2025-10-24 15:54:52 +0100

    Clean ignored files and update .gitignore

--- src/routes/DashboardsRoutes.jsx
+++ src/routes/DashboardsRoutes.jsx
@@ -3,7 +3,6 @@ import ProtectedRoute from "../components/ProtectedRoute";
 
 import SuperAdminDashboard from "../dashboards/SuperAdmin/SuperAdminDashboard";
 import AdminManagement from "../dashboards/SuperAdmin/AdminManagement";
-// import AgentManagement from "../dashboards/SuperAdmin/AgentManagement";
 
 // ================== ADMIN DASHBOARD ==================
 import AdminDashboard from "../dashboards/AdminDashboard/AdminDashboard";
@@ -18,23 +17,11 @@ import AgentClients from "../dashboards/AgentDashboard/Clients";
 import AgentSettings from "../dashboards/AgentDashboard/Settings";
 import AgentPayments from "../dashboards/AgentDashboard/Payment";
 
-// ================== HANDYMAN DASHBOARD ==================
-// import WorkerDashboard from "../dashboards/WorkersDashboard/WorkersDashboard";
-// import WorkerOverview from "../dashboards/WorkersDashboard/Overview";
-// import WorkerProjects from "../dashboards/WorkersDashboard/Projects";
-// import WorkerClients from "../dashboards/WorkersDashboard/Clients";
-// import WorkerAgents from "../dashboards/WorkersDashboard/Agents";
-// import WorkerPayments from "../dashboards/WorkersDashboard/Payments";
-// import WorkerSettings from "../dashboards/WorkersDashboard/Settings";
 
 // ================== VISITOR DASHBOARD ==================
 import VisitorDashboard from "../dashboards/VisitorDashboard/VisitorDashboard";
 import VisitorOverview from "../dashboards/VisitorDashboard/Overview";
 import VisitorProperties from "../dashboards/VisitorDashboard/Properties";
-// import VisitorServices from "../dashboards/VisitorDashboard/Workers";
-// import VisitorSettings from "../dashboards/VisitorDashboard/Settings";
-// import VisitorPayments from "../dashboards/VisitorDashboard/Payments";
-// import VisitorAgents from "../dashboards/VisitorDashboard/Agents";
 
 // ================== ROUTES ==================
 const dashboardRoutes = [
@@ -52,7 +39,6 @@ const dashboardRoutes = [
       { index: true, element: <Navigate to="overview" replace /> },
       { path: "overview/*", element: <AdminOverview /> },
       { path: "admins/", element: <AdminManagement /> },
-      // { path: "agents/", element: <AgentManagement /> },
     ],
   },
   // ===== Admin =====
@@ -88,24 +74,6 @@ const dashboardRoutes = [
     ],
   },
 
-  // ===== Handyman =====
-  // {
-  //   path: "handyman-dashboard",
-  //   element: (
-  //     <ProtectedRoute allowedRoles={["handyman"]}>
-  //       <WorkerDashboard />
-  //     </ProtectedRoute>
-  //   ),
-  //   children: [
-  //     { index: true, element: <Navigate to="overview" replace /> },
-  //     { path: "overview/*", element: <WorkerOverview /> },
-  //     { path: "projects/*", element: <WorkerProjects /> },
-  //     { path: "clients/*", element: <WorkerClients /> },
-  //     { path: "agents/*", element: <WorkerAgents /> },
-  //     { path: "payments/*", element: <WorkerPayments /> },
-  //     { path: "settings/*", element: <WorkerSettings /> },
-  //   ],
-  // },
 
   // ===== Visitor =====
   {
@@ -119,10 +87,6 @@ const dashboardRoutes = [
       { index: true, element: <Navigate to="overview" replace /> },
       { path: "overview/*", element: <VisitorOverview /> },
       { path: "properties/*", element: <VisitorProperties /> },
-      // { path: "workers/*", element: <VisitorServices /> },
-      // { path: "agents/*", element: <VisitorAgents /> },
-      // { path: "settings/*", element: <VisitorSettings /> },
-      // { path: "payments/*", element: <VisitorPayments /> },
     ],
   },
 ];
