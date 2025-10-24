8d7ac907d0909fa7b6eb479e1721954e15d47913
AnietieDaniel <anietienteabasi@gmail.com>
2025-10-24 12:50:38 +0100

    Clean ignored files and update .gitignore

--- src/routes/DashboardsRoutes.jsx
+++ src/routes/DashboardsRoutes.jsx
@@ -3,7 +3,7 @@ import ProtectedRoute from "../components/ProtectedRoute";
 
 import SuperAdminDashboard from "../dashboards/SuperAdmin/SuperAdminDashboard";
 import AdminManagement from "../dashboards/SuperAdmin/AdminManagement";
-import AgentManagement from "../dashboards/SuperAdmin/AgentManagement";
+// import AgentManagement from "../dashboards/SuperAdmin/AgentManagement";
 
 // ================== ADMIN DASHBOARD ==================
 import AdminDashboard from "../dashboards/AdminDashboard/AdminDashboard";
@@ -19,22 +19,22 @@ import AgentSettings from "../dashboards/AgentDashboard/Settings";
 import AgentPayments from "../dashboards/AgentDashboard/Payment";
 
 // ================== HANDYMAN DASHBOARD ==================
-import WorkerDashboard from "../dashboards/WorkersDashboard/WorkersDashboard";
-import WorkerOverview from "../dashboards/WorkersDashboard/Overview";
-import WorkerProjects from "../dashboards/WorkersDashboard/Projects";
-import WorkerClients from "../dashboards/WorkersDashboard/Clients";
-import WorkerAgents from "../dashboards/WorkersDashboard/Agents";
-import WorkerPayments from "../dashboards/WorkersDashboard/Payments";
-import WorkerSettings from "../dashboards/WorkersDashboard/Settings";
+// import WorkerDashboard from "../dashboards/WorkersDashboard/WorkersDashboard";
+// import WorkerOverview from "../dashboards/WorkersDashboard/Overview";
+// import WorkerProjects from "../dashboards/WorkersDashboard/Projects";
+// import WorkerClients from "../dashboards/WorkersDashboard/Clients";
+// import WorkerAgents from "../dashboards/WorkersDashboard/Agents";
+// import WorkerPayments from "../dashboards/WorkersDashboard/Payments";
+// import WorkerSettings from "../dashboards/WorkersDashboard/Settings";
 
 // ================== VISITOR DASHBOARD ==================
 import VisitorDashboard from "../dashboards/VisitorDashboard/VisitorDashboard";
 import VisitorOverview from "../dashboards/VisitorDashboard/Overview";
 import VisitorProperties from "../dashboards/VisitorDashboard/Properties";
-import VisitorServices from "../dashboards/VisitorDashboard/Workers";
-import VisitorSettings from "../dashboards/VisitorDashboard/Settings";
-import VisitorPayments from "../dashboards/VisitorDashboard/Payments";
-import VisitorAgents from "../dashboards/VisitorDashboard/Agents";
+// import VisitorServices from "../dashboards/VisitorDashboard/Workers";
+// import VisitorSettings from "../dashboards/VisitorDashboard/Settings";
+// import VisitorPayments from "../dashboards/VisitorDashboard/Payments";
+// import VisitorAgents from "../dashboards/VisitorDashboard/Agents";
 
 // ================== ROUTES ==================
 const dashboardRoutes = [
@@ -52,7 +52,7 @@ const dashboardRoutes = [
       { index: true, element: <Navigate to="overview" replace /> },
       { path: "overview/*", element: <AdminOverview /> },
       { path: "admins/", element: <AdminManagement /> },
-      { path: "agents/", element: <AgentManagement /> },
+      // { path: "agents/", element: <AgentManagement /> },
     ],
   },
   // ===== Admin =====
@@ -89,23 +89,23 @@ const dashboardRoutes = [
   },
 
   // ===== Handyman =====
-  {
-    path: "handyman-dashboard",
-    element: (
-      <ProtectedRoute allowedRoles={["handyman"]}>
-        <WorkerDashboard />
-      </ProtectedRoute>
-    ),
-    children: [
-      { index: true, element: <Navigate to="overview" replace /> },
-      { path: "overview/*", element: <WorkerOverview /> },
-      { path: "projects/*", element: <WorkerProjects /> },
-      { path: "clients/*", element: <WorkerClients /> },
-      { path: "agents/*", element: <WorkerAgents /> },
-      { path: "payments/*", element: <WorkerPayments /> },
-      { path: "settings/*", element: <WorkerSettings /> },
-    ],
-  },
+  // {
+  //   path: "handyman-dashboard",
+  //   element: (
+  //     <ProtectedRoute allowedRoles={["handyman"]}>
+  //       <WorkerDashboard />
+  //     </ProtectedRoute>
+  //   ),
+  //   children: [
+  //     { index: true, element: <Navigate to="overview" replace /> },
+  //     { path: "overview/*", element: <WorkerOverview /> },
+  //     { path: "projects/*", element: <WorkerProjects /> },
+  //     { path: "clients/*", element: <WorkerClients /> },
+  //     { path: "agents/*", element: <WorkerAgents /> },
+  //     { path: "payments/*", element: <WorkerPayments /> },
+  //     { path: "settings/*", element: <WorkerSettings /> },
+  //   ],
+  // },
 
   // ===== Visitor =====
   {
@@ -119,10 +119,10 @@ const dashboardRoutes = [
       { index: true, element: <Navigate to="overview" replace /> },
       { path: "overview/*", element: <VisitorOverview /> },
       { path: "properties/*", element: <VisitorProperties /> },
-      { path: "workers/*", element: <VisitorServices /> },
-      { path: "agents/*", element: <VisitorAgents /> },
-      { path: "settings/*", element: <VisitorSettings /> },
-      { path: "payments/*", element: <VisitorPayments /> },
+      // { path: "workers/*", element: <VisitorServices /> },
+      // { path: "agents/*", element: <VisitorAgents /> },
+      // { path: "settings/*", element: <VisitorSettings /> },
+      // { path: "payments/*", element: <VisitorPayments /> },
     ],
   },
 ];
