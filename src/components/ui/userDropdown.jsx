198280faa36a695b31272a52692b6eadfce4e27a
David Daniel <144479940+Anietiedaniel@users.noreply.github.com>
2026-07-09 13:21:45 +0000

    modification  done

--- src/components/ui/userDropdown.jsx
+++ src/components/ui/userDropdown.jsx
@@ -82,7 +82,7 @@ const UserDropdown = ({ user, logout }) => {
             className="block w-full text-left px-4 py-2 text-sm text-red-600 
                        hover:bg-red-50 border-t"
           >
-            ⏻ Logout
+            Logout
           </button>
         </div>
       )}
